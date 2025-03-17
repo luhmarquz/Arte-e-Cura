eb5932e2df35016e60fd75d7488a9a848bc4d4de
Luísa Marques <137580816+luhmarquz@users.noreply.github.com>
2025-03-17 16:47:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+document.querySelector('.folder').oncontextmenu = function() {
+    return false; 
+};
+
+//login normal
+function togglePassword() {
+    const senhaInput = document.getElementById('senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    } else {
+        senhaInput.type = 'password';
+    }
+}
+
+//cadastro normal
+function togglePassword() {
+    const senhaInput = document.getElementById('senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    } else {
+        senhaInput.type = 'password';
+    }
+}
+
+//cadastro profissional
+function togglePassword() {
+    const senhaInput = document.getElementById('senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    } else {
+        senhaInput.type = 'password';
+    }
+}
+
+//login profissional
+function togglePassword() {
+    const senhaInput = document.getElementById('senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    } else {
+        senhaInput.type = 'password';
+    }
+}
